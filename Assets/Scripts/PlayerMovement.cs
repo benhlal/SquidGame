422141bf7bbb35f44e3287698d276052bfa79d3b
benhlal <benhlalyounes@gmail.com>
2021-10-10 20:15:33 +0200

    squid-game-unity3D-first-build

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 100f;
+
+    private float verticalDirection;
+
+    private Rigidbody rb;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = Vector3.forward * verticalDirection * movementSpeed * Time.fixedDeltaTime;
+    }
+
+    void Update()
+    {
+        verticalDirection = Input.GetAxis("Vertical");
+        verticalDirection = Mathf.Clamp(verticalDirection, 0, 1);
+        animator.SetFloat("Speed", verticalDirection);
+    }
+}
